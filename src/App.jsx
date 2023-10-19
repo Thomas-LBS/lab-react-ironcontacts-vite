43a902afe6a3bfa9b47bed4e0cdfff022d4376ba
Thomas Lebas <thomas.lebas26@gmail.com>
2023-10-19 11:35:34 +0200

    random and delete + style

--- src/App.jsx
+++ src/App.jsx
@@ -3,76 +3,116 @@ import { useState } from 'react';
 import contacts from "./contacts.json"
 
 function App() {
-  
-  const fiveContacts = contacts
-  
-  
+  const firstFiveActors = contacts.slice(0, 5);
+  const [contactsData, setContactData] = useState(firstFiveActors);
+
+  {/* Function Add Random Contact */ }
+  function handleClickRandom(array) {
+
+    const minArrayLength = 5
+    const maxArrayLength = array.length
+
+    if (maxArrayLength <= minArrayLength) {
+      return -1
+    }
+
+    const randomContact = Math.floor(Math.random() * (maxArrayLength - minArrayLength) + minArrayLength)
+
+    setContactData((actualState) => {
+      const updatedState = [...actualState]
+
+      updatedState.push(contacts[randomContact])
+
+      return updatedState
+    })
+  }
+
+
+  {/* Function Sort by Name */ }
+
+
+  {/* Function Sort by Popularity */ }
+
+
+  {/* Function Delete Contact */ }
+  function handleClickDelete(actorId) {
+    setContactData((actualState) => {
+      const updatedState = [...actualState]
+      return updatedState.filter((actor) => actor.id !== actorId)
+    })
+  }
+
+
   return (
     <div className="App">
-      <h1>IronContacts</h1>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-            <th>Won an Oscar</th>
-            <th>Won an Emmy</th>
-          </tr>
-        </thead>
+      <div className="title">
+        <h1>IronContacts</h1>
+      </div>
+
+
+      {/* buttons bar */}
+
+      <div className="sticky">
+        <div className="button">
+          <button className="randomBtn" onClick={() => handleClickRandom(contacts)}>
+            Add Random Contact
+          </button>
+          <button className="randomBtn" onClick={() => handleClickSortName()}>
+            Sort by Name
+          </button>
+          <button className="randomBtn" onClick={() => handleClickSortPopularity()}>
+            Sort by Popularity
+          </button>
+        </div>
+       
+          <table>
+            <tbody>
+              <tr className="title">
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                <th>Won Oscar</th>
+                <th> Won Emmy</th>
+                <th>Action</th>
+              </tr>
+            </tbody>
+          </table>
+        
+      </div>
+
+
+
+      {/* Actors Table */}
+      <table className="actorsTb">
         <tbody>
-        {/*Contact 1*/}
-          <tr>
-            <td><img src={fiveContacts[0].pictureUrl} alt="pic" style={{ width: "50px" }} /></td>
-            <td>{fiveContacts[0].name}</td>
-            <td>{fiveContacts[0].popularity.toFixed(2)}</td>
-            <td>{fiveContacts[0].wonOscar && <span>🏆</span>}</td>
-            <td>{fiveContacts[0].wonEmmy && <span>🌟</span>}</td>
-          </tr>
+          {contactsData.map((actor, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <img
+                    src={actor.pictureUrl}
+                    alt=""
+                    style={{ width: "75px" }}
+                  />
+                </td>
+                <td>{actor.name}</td>
+                <td>{actor.popularity.toFixed(2)}</td>
+                {actor.wonOscar ? <td><span className="glory">🏆</span><span className="glory">🏆</span><span className="glory">🏆</span></td> : <td></td>}
+                {actor.wonEmmy ? <td><span className="glory">🌟</span><span className="glory">🌟</span><span className="glory">🌟</span></td> : <td></td>}
+                <td>
+                  <button className="randomBtn" onClick={() => handleClickDelete(actor.id)}>Delete</button>
+                </td>
 
-          {/*Contact 2*/} 
-          <tr>
-            <td><img src={fiveContacts[1].pictureUrl} alt="pic" style={{ width: "50px" }} /></td>
-            <td>{fiveContacts[1].name}</td>
-            <td>{fiveContacts[1].popularity.toFixed(2)}</td>
-            <td>{fiveContacts[1].wonOscar && <span>🏆</span>}</td>
-            <td>{fiveContacts[1].wonEmmy && <span>🌟</span>}</td>
-          </tr>
-
-          {/*Contact 3*/} 
-          <tr>
-            <td><img src={fiveContacts[2].pictureUrl} alt="pic" style={{ width: "50px" }} /></td>
-            <td>{fiveContacts[2].name}</td>
-            <td>{fiveContacts[2].popularity.toFixed(2)}</td>
-            <td>{fiveContacts[2].wonOscar && <span>🏆</span>}</td>
-            <td>{fiveContacts[2].wonEmmy && <span>🌟</span>}</td>
-          </tr>
-
-          {/*Contact 4*/} 
-          <tr>
-            <td><img src={fiveContacts[3].pictureUrl} alt="pic" style={{ width: "50px" }} /></td>
-            <td>{fiveContacts[3].name}</td>
-            <td>{fiveContacts[3].popularity.toFixed(2)}</td>
-            <td>{fiveContacts[3].wonOscar && <span>🏆</span>}</td>
-            <td>{fiveContacts[3].wonEmmy && <span>🌟</span>}</td>
-          </tr>
-
-          {/*Contact 5*/} 
-          <tr>
-            <td><img src={fiveContacts[4].pictureUrl} alt="pic" style={{ width: "50px" }} /></td>
-            <td>{fiveContacts[4].name}</td>
-            <td>{fiveContacts[4].popularity.toFixed(2)}</td>
-            <td>{fiveContacts[4].wonOscar && <span>🏆</span>}</td>
-            <td>{fiveContacts[4].wonEmmy && <span>🌟</span>}</td>
-          </tr>
-
-
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-
     </div>
   );
 }
 
+
 export default App;
+
